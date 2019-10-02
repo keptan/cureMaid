2da266e461ad94d20047c8162eb00f841fb1411e
keptan <keptan@protonmail.com>
2019-10-02 12:33:10 -1000

    buildDatabases transactional test

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,32 @@
 #include "transactional.h"
 
+void buildDatabases (Database& db)
+{
+	auto t = db.transaction();
+	//image identity database, holds hashes of all the images....
+	db.CREATE("TABLE IF NOT EXISTS images (hash STRING NOT_NULL PRIMARY_KEY)");
+
+	//identity skills 
+	db.CREATE("TABLE IF NOT EXISTS idScore (hash STRING NOT_NULL PRIMARY_KEY REFERENCES identity(hash),"
+											"mu REAL NOT_NULL, sigma REAL NOT_NULL)");
+	//tag identity
+	//table that holds all the tags
+	db.CREATE("TABLE IF NOT EXISTS tags (tag STRING NOT_NULL PRIMARY KEY)");
+
+	//tags to image bridge 
+	db.CREATE("TABLE IF NOT EXISTS image_tag_bridge (hash STRING NOT_NULL REFERENCES images,"
+													"tag STRING NOT_NULL REFERENCES tags)");
+
+
+	//only commit if we can make all the tables..
+	t.commit();
+}
+
+
+
+	
 int main (void)
 {
-	return test();
+	Database db ("../test");
+	buildDatabases(db);
 }
