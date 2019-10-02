048c7cf5f51528667057869e8062878da36745f8
keptan <keptan@protonmail.com>
2019-10-01 15:57:00 -1000

    test the insert, update, create commands

--- lib/transactional/transactional.cpp
+++ lib/transactional/transactional.cpp
@@ -2,19 +2,28 @@
 
 int test (void)
 {
-	Database db ("test");
+	Database db ("../test");
 	for(const auto [name, age] : db.SELECT<std::string, double> ("* from ages; DROP table ages;")) 
 	std::cout << name << ' ' << age << '\n';
 
 	auto transaction = db.transaction();
 	auto stmt = db.INSERT("INTO ages (name, age) VALUES (?, ?)");
+	auto updt = db.UPDATE("ages SET age = 99 WHERE name = ?");
+	updt.push("dogg");
 
-	if(db.SELECT<std::string, double>("* from ages where name = 'promotion'").size() == 0)
+	if(db.SELECT<std::string, double>("* from ages where name = 'dogg'").size() == 0)
 	{
-		std::cout << "inserting PROMOTION" << std::endl;
-		stmt.push("promotion", 10.5);
-		transaction.commit();
+		std::cout << "inserting DOG" << std::endl;
+		stmt.push("dogg", 10.5);
 	}
+
+	db.CREATE("TABLE IF NOT EXISTS parts ("
+			"name STRING NOT_NULL PRIMARY_KEY,"
+			"price NUMBER NOT_NULL)");
+
+
+	transaction.commit();
+	//transaction.commit();
 	//will be rolledback automatically on scope close
 	//
 }
